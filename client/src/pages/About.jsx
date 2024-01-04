b49adad8067a8fa07ed2ce67e0fa3e3f2fb73dce
Amene Terhemen <ameneterh@gmail.com>
2024-01-04 17:52:37 +0100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,42 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-justify">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Starlight Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              corporis tempora ullam aut ipsam facere vel quis nostrum nisi odit
+              saepe unde aliquid, debitis possimus soluta dignissimos eum, ea
+              doloremque! Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Necessitatibus similique magnam quibusdam ullam quidem
+              alias! Cupiditate atque modi veritatis? Id inventore, provident
+              animi laboriosam neque aperiam error cumque blanditiis distinctio.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sed
+              voluptatum, officia accusamus veniam iste laudantium
+              exercitationem velit iure aliquam accusantium deleniti inventore
+              tenetur! Praesentium odio ab cupiditate fugit inventore?Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Nemo rerum
+              voluptas iure. Praesentium officiis culpa, deleniti libero
+              obcaecati minima deserunt vero eligendi incidunt cupiditate
+              molestiae, ab sit, numquam error laboriosam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus sit harum, nobis aspernatur error rerum vel aperiam
+              velit possimus iure, consequuntur quaerat quam animi! Porro vitae
+              molestiae quo ab facilis.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
