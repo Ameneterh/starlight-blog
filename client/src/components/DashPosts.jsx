6b6bcda5f2bb9a99004bf4bf7b6cc4a3453dfe1f
amspharmacy <ameneterh@gmail.com>
2024-06-09 22:58:10 +0100

    edit picture position to dashpost page

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -103,7 +103,7 @@ export default function DashPosts() {
                       <img
                         src={post.image}
                         alt={post.title}
-                        className="w-20 h-10 object-cover bg-gray-500"
+                        className="w-20 h-10 object-cover object-top bg-gray-500"
                       />
                     </Link>
                   </Table.Cell>
