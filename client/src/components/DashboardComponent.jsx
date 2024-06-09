78a3db54bb684f59760d0b0414019422a1b20bc7
amspharmacy <ameneterh@gmail.com>
2024-06-09 17:56:00 +0100

    updated dashboard component

--- client/src/components/DashboardComponent.jsx
+++ client/src/components/DashboardComponent.jsx
@@ -132,7 +132,7 @@ export default function DashboardComponent() {
       </div>
       {/* tables */}
       <div className="flex flex-wrap gap-4 py-3 mx-auto justify-center">
-        <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
+        <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800 flex-1">
           <div className="flex justify-between p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent Users</h1>
             <Button outline gradientDuoTone="purpleToPink">
@@ -163,7 +163,7 @@ export default function DashboardComponent() {
         </div>
 
         {/* second */}
-        <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
+        <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800 flex-1">
           <div className="flex justify-between p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent Comments</h1>
             <Button outline gradientDuoTone="purpleToPink">
@@ -188,39 +188,38 @@ export default function DashboardComponent() {
               ))}
           </Table>
         </div>
-
-        {/* third */}
-        <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
-          <div className="flex justify-between p-3 text-sm font-semibold">
-            <h1 className="text-center p-2">Recent Posts</h1>
-            <Button outline gradientDuoTone="purpleToPink">
-              <Link to={"/dashboard?tab=posts"}>See all</Link>
-            </Button>
-          </div>
-          <Table hoverable>
-            <Table.Head>
-              <Table.HeadCell>Post Image</Table.HeadCell>
-              <Table.HeadCell>Post Title</Table.HeadCell>
-              <Table.HeadCell>Category</Table.HeadCell>
-            </Table.Head>
-            {posts &&
-              posts.map((post) => (
-                <Table.Body key={post._id} className="divide-y">
-                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell>
-                      <img
-                        src={post.image}
-                        alt="user"
-                        className="w-14. h-10 rounded-md bg-gray-500"
-                      />
-                    </Table.Cell>
-                    <Table.Cell className="w-96">{post.title}</Table.Cell>
-                    <Table.Cell className="w-5">{post.category}</Table.Cell>
-                  </Table.Row>
-                </Table.Body>
-              ))}
-          </Table>
+      </div>
+      {/* third - SHOWING RECENT POSTS*/}
+      <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800 flex-1">
+        <div className="flex justify-between p-3 text-sm font-semibold">
+          <h1 className="text-center p-2">Recent Posts</h1>
+          <Button outline gradientDuoTone="purpleToPink">
+            <Link to={"/dashboard?tab=posts"}>See all</Link>
+          </Button>
         </div>
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>Post Image</Table.HeadCell>
+            <Table.HeadCell>Post Title</Table.HeadCell>
+            <Table.HeadCell>Category</Table.HeadCell>
+          </Table.Head>
+          {posts &&
+            posts.map((post) => (
+              <Table.Body key={post._id} className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    <img
+                      src={post.image}
+                      alt="user"
+                      className="w-14. h-10 rounded-md bg-gray-500"
+                    />
+                  </Table.Cell>
+                  <Table.Cell className="w-96">{post.title}</Table.Cell>
+                  <Table.Cell className="w-5">{post.category}</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+        </Table>
       </div>
     </div>
   );
